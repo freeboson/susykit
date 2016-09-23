64b4aaaaa82d0d060bbc3a3941c12c3e40bc9377
Sujeet Akula <sujeet@freeboson.org>
2016-09-23 14:39:46 +1000

    these are defunct. why are they even here?

--- src/old-scanner/susykit_init.cpp
+++ src/old-scanner/susykit_init.cpp
@@ -1,5 +1,34 @@
 /*
 
+    ----------------------------------------------------------------------------
+    -                                                                          -
+    - Copyright 2011-2016 Sujeet Akula                                         -
+    - sujeet@freeboson.org                                                     -
+    -                                                                          -
+    - susykit_init.cpp: This is a defunct component.                           -
+    -                                                                          -
+    - This file is part of SusyKit, https://freeboson.org/susykit/.            -
+    -                                                                          -
+    - SusyKit is free software: you can redistribute it and/or modify it under -
+    - the terms of the GNU Lesser General Public License as published by the   -
+    - Free Software Foundation, either version 3 of the License, or (at your   -
+    - option) any later version.                                               -
+    -                                                                          -
+    - SusyKit is distributed in the hope that it will be useful, but WITHOUT   -
+    - ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    -
+    - FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public     -
+    - License for more details.                                                -
+    -                                                                          -
+    - You should have received a copy of the GNU Lesser General Public License -
+    - along with SusyKit.  If not, see <http://www.gnu.org/licenses/>.         -
+    -                                                                          -
+    ----------------------------------------------------------------------------
+
+*/
+
+
+/*
+
    **************************************************************
    *                                                            *
    * SOFTSUSY Large Mu Scanner                                  *
