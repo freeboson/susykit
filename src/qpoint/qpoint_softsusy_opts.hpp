acd5b9b478bd984b635a7884fde0daccf63cdfe8
Sujeet Akula <sujeet@freeboson.org>
2016-09-23 14:32:16 +1000

    add lgpl header for release

--- src/qpoint/qpoint_softsusy_opts.hpp
+++ src/qpoint/qpoint_softsusy_opts.hpp
@@ -1,3 +1,32 @@
+/*
+
+    ----------------------------------------------------------------------------
+    -                                                                          -
+    - Copyright 2011-2016 Sujeet Akula                                         -
+    - sujeet@freeboson.org                                                     -
+    -                                                                          -
+    - qpoint_softsusy_opts.hpp:                                                -
+    -                                                                          -
+    - This file is part of SusyKit, https://freeboson.org/susykit/.            -
+    -                                                                          -
+    - SusyKit is free software: you can redistribute it and/or modify it under -
+    - the terms of the GNU Lesser General Public License as published by the   -
+    - Free Software Foundation, either version 3 of the License, or (at your   -
+    - option) any later version.                                               -
+    -                                                                          -
+    - SusyKit is distributed in the hope that it will be useful, but WITHOUT   -
+    - ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    -
+    - FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public     -
+    - License for more details.                                                -
+    -                                                                          -
+    - You should have received a copy of the GNU Lesser General Public License -
+    - along with SusyKit.  If not, see <http://www.gnu.org/licenses/>.         -
+    -                                                                          -
+    ----------------------------------------------------------------------------
+
+*/
+
+
 #pragma once
 #ifndef QPOINT_SOFTSUSY_OPTS_HPP
 #define QPOINT_SOFTSUSY_OPTS_HPP
