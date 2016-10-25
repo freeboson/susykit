a6c39d70a9211f203b78ef4b18f9351287556453
Sujeet Akula <sujeet@freeboson.org>
2016-10-25 16:02:16 +1100

    use emplace_back for unique_ptrs

--- include/constrain/loglike.hpp
+++ include/constrain/loglike.hpp
@@ -39,12 +39,12 @@ class hepstats::loglike {
 public:
 
     void add_like_term(std::unique_ptr<likedatum> datum) {
-        like_terms.push_back(std::move(datum));
+        like_terms.emplace_back(std::move(datum));
     }
 
     double get_log_like(const model &m) const;
 
-    unsigned int get_num_like_terms() const { return like_terms.size(); }
+    size_t get_num_like_terms() const { return like_terms.size(); }
 
 private:
     std::list<std::unique_ptr<likedatum> > like_terms;
